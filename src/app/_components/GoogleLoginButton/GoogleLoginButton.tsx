3d834f4ba3ce05471fbb5616922def6f2d7e4391
keitakn <keita.koga.work@gmail.com>
2023-09-20 01:10:03 +0900

    :art: #6 ID連携機能の為にボタンの文言を変更出来るように対応

--- src/app/_components/GoogleLoginButton/GoogleLoginButton.tsx
+++ src/app/_components/GoogleLoginButton/GoogleLoginButton.tsx
@@ -4,10 +4,14 @@ import { signIn } from 'next-auth/react';
 import type { JSX, MouseEvent } from 'react';
 
 type Props = {
+  buttonText?: string;
   callbackUrl?: string;
 };
 
-export const GoogleLoginButton = ({ callbackUrl }: Props): JSX.Element => {
+export const GoogleLoginButton = ({
+  buttonText = 'sign in with Google',
+  callbackUrl,
+}: Props): JSX.Element => {
   const handleLogin = async (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
 
@@ -41,7 +45,7 @@ export const GoogleLoginButton = ({ callbackUrl }: Props): JSX.Element => {
           d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
         ></path>
       </svg>
-      sign in with Google
+      {buttonText}
     </button>
   );
 };
